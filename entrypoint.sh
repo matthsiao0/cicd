632b78755a23593e5c816a3714754616085f55e6
matthsiao0 <matt.hsiao@swag.live>
2023-03-10 14:01:37 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
+chmod +x /entrypoint.sh
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "$time" >> $GITHUB_STEP_SUMMARY
-git add entrypoint.sh
-git update-index --chmod=+x entrypoint.sh
